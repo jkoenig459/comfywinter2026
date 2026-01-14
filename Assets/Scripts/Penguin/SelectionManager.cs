94f7813f42fa2987d4278feb472eac3fd3edad10
Josh <relic1337@gmail.com>
2026-01-14 13:35:30 -0500

    Bugfix: added layer mask to resources so penguin can always be selected

--- Assets/Scripts/Penguin/SelectionManager.cs
+++ Assets/Scripts/Penguin/SelectionManager.cs
@@ -7,11 +7,22 @@ public class SelectionManager : MonoBehaviour
     private Camera cam;
     private Selectable selected;
 
+    // Cache the mask that ignores the Resources layer
+    private int selectionMask;
+
     private void Awake()
     {
-        if (I != null && I != this) { Destroy(gameObject); return; }
+        if (I != null && I != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         I = this;
         cam = Camera.main;
+
+        // Invert the Resources layer so it is ignored
+        selectionMask = ~LayerMask.GetMask("Resources");
     }
 
     private void Update()
@@ -22,30 +33,38 @@ public class SelectionManager : MonoBehaviour
 
     private void TrySelectUnderMouse()
     {
-
         if (cam == null) cam = Camera.main;
+
         Vector2 world = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        var hit = Physics2D.Raycast(world, Vector2.zero);
+        RaycastHit2D hit = Physics2D.Raycast(
+            world,
+            Vector2.zero,
+            0f,
+            selectionMask
+        );
 
         Debug.Log($"Click at world {world}, hit: {(hit.collider ? hit.collider.name : "none")}");
 
         if (!hit.collider)
         {
-
             SetSelected(null);
             return;
         }
 
-        var sel = hit.collider.GetComponentInParent<Selectable>();
+        Selectable sel = hit.collider.GetComponentInParent<Selectable>();
         SetSelected(sel);
     }
 
     private void SetSelected(Selectable sel)
     {
-        if (selected != null) selected.SetSelected(false);
+        if (selected != null)
+            selected.SetSelected(false);
+
         selected = sel;
-        if (selected != null) selected.SetSelected(true);
+
+        if (selected != null)
+            selected.SetSelected(true);
     }
 
     public GameObject SelectedObject => selected ? selected.gameObject : null;
